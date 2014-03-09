e6cf93db9ec0c744557f1d1a8febd77996a6142b
Alexander Badrenkov <sbadrenkov@berkeley.edu>
2014-03-09 15:04:48 -0700

    added tests

--- ex48/tests/parser_tests.py
+++ ex48/tests/parser_tests.py
@@ -3,5 +3,15 @@ from ex48.parser import *
 
 
 def test_parse_sentence():
-	assert_equal(parse_sentence([('noun', 'princess'), ('verb', 'go'), ('direction', 'north')].getSubject()), \
-	('princess'))
+	assert_equal(parse_sentence([('noun', 'princess'), ('verb', 'go'), ('direction', 'north')]).subject, 'princess')
+	assert_equal(parse_sentence([('noun', 'princess'), ('verb', 'go'), ('direction', 'north')]).verb, 'go')
+	assert_equal(parse_sentence([('noun', 'princess'), ('verb', 'go'), ('direction', 'north')]).object, 'north')
+	
+def test_parse_sentence2():
+	assert_equal(parse_sentence([('verb', 'go'), ('direction', 'north')]).subject, 'player')
+	assert_equal(parse_sentence([('verb', 'go'), ('direction', 'north')]).verb, 'go')
+	assert_equal(parse_sentence([('verb', 'go'), ('direction', 'north')]).object, 'north')
+	
+def test_parse_sentence3():
+	assert_raises(ParserError, parse_sentence, [('direction', 'north'), ('verb', 'go'), ('noun', 'princess')])
+	
